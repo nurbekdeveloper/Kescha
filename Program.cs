c1ddea31b1560beabf23099dfc612e9adf2b3e96
nurbekdeveloper <nurbeknormatov_02@gmail.com>
2024-02-04 16:32:35 +0500

    add new files and folder

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int keschaAge = 9;
+
+
+
+
+System.Console.WriteLine("Enter your name ");
+ string first = Console.ReadLine();
+ System.Console.WriteLine($"your name is {first }");
+ System.Console.WriteLine("Enter your age ");
+ string age = Console.ReadLine();
+ System.Console.WriteLine("Converting ");
+ int ageConvert = Convert.ToInt32(age);
+
+
+ System.Console.WriteLine($"your and kesha age difference  is  {ageConvert- keschaAge}");
+
+
+// float width = 2345555;
+// string d  = width.ToString();
+// System.Console.WriteLine(d);
+
+// System.Console.WriteLine(d.GetType());
